08aef5294a2b9af6d008f488a675ff68801f213b
Samuel Brack <samuel.brack@gmail.com>
2014-06-03 15:46:48 +0200

    restructured "vector" for datastructure

---
+++ src/bv_types.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "bv_types.h"
+
+struct Range_borders* range_borders_ctor() {
+    Range_borders* object = (Range_borders*) malloc(sizeof(Range_borders));
+    object->range_borders_max = INIT_SIZE;
+    object->range_borders_current = 0;
+    object->insert_element = Rb_insert_element;
+    object->delete_element = Rb_delete_element;
+    return object;
+}
+
+int Rb_insert_element(struct Range_borders_t* this, int new_element) {
+    if (this->range_borders_current < this->range_borders_max) {
+        this->range_borders[this->range_borders_current++] = new_element;
+    } else {
+        this->range_borders_max = 2 * this->range_borders_max;
+        this->range_borders = (int*)realloc(this->range_borders, (this->range_borders_max) * sizeof(int));
+        this->range_borders[this->range_borders_current++] = new_element;
+    }
+    return 0;
+}
+
+int Rb_delete_element(struct Range_borders_t* this, int index) {
+    if (sizeof(this->range_borders) / sizeof(this->range_borders[0]) > index - 1)
+        return 1;
+    for (int i = index; i < this->range_borders_current; ++i) {
+        this->range_borders[i] = this->range_borders[i + 1];
+    }
+    this->range_borders[this->range_borders_current--] = 0;
+    
+    // check if we can resize the array to half
+    if (this->range_borders_current < this->range_borders_max / 2) {
+        this->range_borders_max /= 2;
+        this->range_borders = (int*)realloc(this->range_borders, (this->range_borders_max) * sizeof(int));
+    }
+    return 0;
+}
--- src/test.c
+++ src/test.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 int main() {
+    Range_borders* obj = range_borders_ctor(INIT_SIZE, 0);
     // test insert function
     for (int i = 0; i < 100; ++i) {
         printf("%d\n", i);
-        insertElement(rangeBorders, i);
+        obj->insert_element(obj, i);
     }
     
     for (int i = 0; i < 100; ++i) {
-        if (rangeBorders[i] == i)
+        if (obj->range_borders[i] == i)
             continue;
         else {
             printf("%s%d%s\n", "Failure at index", i, "!");
